4a3d7f12051246f239dbe3651691f4893e38fdb1
Jake Fulmine <jake@fulmine.xyz>
2025-09-02 11:07:19 +0200

    add to extension tracker

---
+++ config/lorekeeper/ext-tracker/storage_driver.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'wiki_key' => 'Storage_Driver',
+    'creators' => json_encode([
+        'Fulmn' => 'https://github.com/Draconizations/',
+    ]),
+    'version'  => '1.0.0',
+];
