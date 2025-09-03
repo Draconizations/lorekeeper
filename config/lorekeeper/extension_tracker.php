4eae99a03e534073ea6e3ef41b21c2a736a2f141
Jake Fulmine <jake@fulmine.xyz>
2025-09-03 16:30:04 +0200

    bump version

--- config/lorekeeper/extension_tracker.php
+++ config/lorekeeper/extension_tracker.php
@@ -16,7 +16,7 @@ return [
         'creators' => json_encode([
             'Fulmn' => 'https://github.com/Draconizations/'
         ]),
-        'version' => '1.1.1',
+        'version' => '1.1.2',
     ],
 
     // FORMAT EXAMPLE.
